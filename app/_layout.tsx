2582b7316bbacf2eb3bf44c29c47eec94cdd848a
L3sty <klestbegolli@gmail.com>
2026-05-11 11:31:00 +0200

    fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,10 @@
 import { Slot } from 'expo-router';
 import { WishlistProvider } from './context/WishlistContext';
-import { ThemeProvider } from './context/ThemeContext';
 
 export default function RootLayout() {
   return (
     <WishlistProvider>
-            <ThemeProvider>
       <Slot />
-      </ThemeProvider>
     </WishlistProvider>
   );
 }
